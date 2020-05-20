a2400994e8376d8040af8d7be0d4c11530ee5f0c
Kunardi <kunardizhang@gmail.com>
2020-05-21 00:38:10 +0930

    1. add Menu funcntion on the menu bar in all HTML . 2. fix Help.html 3. add manual contrioller on slide shows

--- home.js
+++ home.js
@@ -1,2 +1,35 @@
 const outName = document.getElementById("name");
 outName.innerHTML= localStorage.getItem("Name");
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
